4a6f6e03304f9ff6c6a8c557f4a9299a2200cbbd
h-wholf <kemalmoto.e5@gmail.com>
2021-02-14 12:04:42 -0600

    funciones_maid3

--- MAID.sh
+++ MAID.sh
@@ -1,70 +1,129 @@
 #!/data/data/com.termux/files/usr/bin/zsh
 #usar zsh shell para que funcione
-clear
-num=1
-core2(){
-  archivo=$(ls|wc -l)
-  if [ $num -le $archivo ];
-  then
-  a=$(ls -c|awk NR==$num)
-  c=$(setterm --foreground cyan)
-  cb=$(setterm --foreground white)
-  r=$(setterm --foreground yellow)
-  
 
-  if [ -d "$a" ]
-  then
-   setterm --foreground yellow
-   carpeta="Es Un Directorio"
-   echo $carpeta
-   i=$(echo $a)
-  fi
+num=1
+clear
+#FUNCION DE COLORES DEL TEXTO
+colores(){
+  cy=$(setterm --foreground cyan)
+  wh=$(setterm --foreground white)
+  ye=$(setterm --foreground yellow)
+  gr=$(setterm --foreground green)
+  re=$(setterm --foreground red)
   
-  setterm --foreground white
-  echo 'ARCHIVO NUMERO '$r$num$cb '| '$c'o'$cb 'ABRIR | '$c'x '$cb'SALIR | '$c'b '$cb'BORRAR'
-  setterm --foreground green
-  t=$(du -sh $a|awk '{print $1}')
-  echo $a $cb' TAMAÑO: '$r$t 
-  
-  
-  
-  let num=$num+1
-  else
-  setterm --foreground white
-  echo SON TODOS LOS ARCHIVOS SALIENDO 😎
-  sleep 2;
-  exit
-  fi
-  read b
-  if [ "$b" = x ];
-  then
-  exit
-  fi
-  if [ "$b" = o ];
-  then
-  ruta=$(pwd)
-  q="'"
-  termux-open $a
-  fi
-  if [ "$b" = b ];
-  then 
-      rm $a
-  fi
-  if [ "$b" = c ];
-  then
-  cd $a
-  num=1
-  fi
-  if [ "$b" = a ];
-  then
-  cd ..
-  num=1
-  fi
-  core2
   
   
 }
 
-core2
+
+#FUNCION PRINCIPAL
+init(){
+  n_archivo=$(ls|wc -l)
+  archivo=$(ls -c|awk NR==$num)
+  t=$(du -sh $archivo|awk '{print $1}')
+  if [ -d $archivo ]
+  then 
+  
+  dir="📁  "
+  else 
+  dir=""
+  fi
+  
+  echo $wh'ARCHIVO NUMERO '$cy$num '| '$cy'o'$wh 'ABRIR | '$cy'x '$wh'SALIR | '$cy'b '$wh'BORRAR'
+  
+  echo $dir $gr$archivo $ye'     tamaño : '$cy$t 
+  
+  read accion
+  let num=$num+1
+  $accion
+  init
+}
+
+#FUNCION QUE ABRE el archivo
+o(){
+  
+  echo $ye'esta funcion abrira el archivo'
+  termux-open $archivo
+  
+  init
+}
 
 
+#FUNCION QUE ABRE EL DIRECTORIO selecionado
+c(){
+  echo $cy'esta funcion entrara a la carpeta especificada'
+ cd $archivo
+ num=1
+ init
+}
+
+
+#FUNCION DE SALIDA DEL APP-SCRIPT
+x(){
+  echo esta funcion sale del escript
+  
+  exit
+  
+}
+
+#FUNCION QUE BORRA EL ARCHIVO
+a(){
+  echo esta funcion regresa un direcctorio
+  cd ..
+  num=1
+  init
+}
+
+
+#FUNCION QUE BORRA EL ARCHIVO seleccionado
+b(){
+  rm $archivo
+  let num=$num-1
+  init
+  
+}
+
+#FUNCION PROMPT QUE PERMITE METER COMANDOS
+p(){
+  echo esta funcion permite meter comandos y acceder al promt de bash
+  read prompt
+  $prompt
+  init
+  
+}
+
+
+#FUNCION QUE PERMITE EDITAR ARCHIVOS
+v(){
+  vim $archivo
+  let num=$num-1
+  init
+  
+  
+}
+
+
+w(){
+ echo un pazo atras
+ n_archivo=$(ls|wc -l)
+  archivo=$(ls -c|awk NR==$num)
+  t=$(du -sh $archivo|awk '{print $1}')
+  echo 'ARCHIVO NUMERO '$num '| '$cy'o'$wh 'ABRIR | '$cy'x '$wh'SALIR | '$cy'b '$wh'BORRAR'
+  
+  echo $gr$archivo $ye'     tamaño : '$cy$t
+  read accion
+  let num=$num-1
+  $accion
+  w
+  
+  
+}
+i(){
+  init
+  
+  
+}
+
+#INICIALIZAR ENGINE
+colores
+init
